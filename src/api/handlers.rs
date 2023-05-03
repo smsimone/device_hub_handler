a197c75a827584328c18fa77c254bc7cda36270a
smaso <smsimone65@gmail.com>
2023-05-03 15:04:42 +0200

    feat: only allowing zip files to be uploaded

--- src/api/handlers.rs
+++ src/api/handlers.rs
@@ -1,4 +1,9 @@
-use std::{path::Path, thread};
+use std::{
+    fs::{create_dir_all, read_dir, DirEntry},
+    io::Cursor,
+    path::Path,
+    thread,
+};
 
 use axum::{extract::Multipart, http::StatusCode, response::Response, routing::post, Router};
 use log::info;
@@ -46,28 +51,80 @@ async fn upload_bundle(mut multipart: Multipart) -> Result<Response, StatusCode>
 
         let temp_file_path = format!("{}/{}", &download_dir, filename);
 
-        if Path::new(&temp_file_path).exists() {
+        let temp_file = Path::new(&temp_file_path);
+
+        let extension = match temp_file.extension() {
+            Some(extension) => {
+                let ext = extension.to_str().unwrap().to_string();
+                if ext != "zip" {
+                    error!("Only zip files are supported. Received {}", &ext);
+                    return Err(StatusCode::BAD_REQUEST);
+                }
+                ext
+            }
+            None => {
+                error!("Missing extension in file {}", &filename);
+                return Err(StatusCode::UNPROCESSABLE_ENTITY);
+            }
+        };
+
+        if temp_file.exists() {
             match std::fs::remove_file(&temp_file_path) {
                 Ok(_) => info!("Already existent file {} has been removed", &filename),
                 Err(err) => error!("Couldn't remove file {}:\n{}", &filename, err.to_string()),
             }
         }
 
-        match std::fs::write(&temp_file_path, &bytes) {
-            Ok(_) => {
-                info!(
-                    "Written file {}. Starting with the installation process",
-                    &filename
-                );
-                thread::spawn(move || match install_bundle_all(&temp_file_path) {
-                    Ok(_) => info!("Installed bundle againts all devices"),
-                    Err(err) => error!("Failed to install bundle:\n{}", err),
-                });
-            }
+        let extraction_folder = temp_file
+            .parent()
+            .unwrap()
+            .join(filename.replace(&format!(".{}", extension), ""))
+            .join("extract");
+
+        info!(
+            "Extracting zip in folder: {}",
+            &extraction_folder.to_str().unwrap().to_string()
+        );
+
+        match create_dir_all(&extraction_folder) {
+            Ok(_) => info!("Created all directories to the extraction path"),
             Err(err) => {
-                error!("Failed to write file {}:\n{}", &filename, err.to_string());
+                error!(
+                    "Failed to create directories in path to extraction folder:\n{}",
+                    err.to_string()
+                );
+                return Err(StatusCode::INTERNAL_SERVER_ERROR);
+            }
+        }
+
+        match zip_extract::extract(Cursor::new(&bytes), &extraction_folder, true) {
+            Ok(_) => info!(
+                "Extracted zip file in {}",
+                &extraction_folder.to_str().unwrap()
+            ),
+            Err(err) => {
+                error!("Failed to extract zip file:\n{}", err.to_string());
                 return Err(StatusCode::UNPROCESSABLE_ENTITY);
             }
+        }
+
+        let entries = match read_dir(&extraction_folder) {
+            Ok(e) => e
+                .filter(|item| item.is_ok())
+                .map(|item| item.unwrap())
+                .collect::<Vec<DirEntry>>(),
+            Err(err) => {
+                error!("Failed to read extraction directory: {}", err.to_string());
+                return Err(StatusCode::INTERNAL_SERVER_ERROR);
+            }
+        };
+
+        for bundle_file in entries {
+            let path = bundle_file.path().to_str().unwrap().to_string();
+            thread::spawn(move || match install_bundle_all(&path) {
+                Ok(_) => info!("Installed bundle againts all devices"),
+                Err(err) => error!("Failed to install bundle:\n{}", err),
+            });
         }
     }
     return Ok(Response::default());
